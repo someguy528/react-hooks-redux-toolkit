023aedd42c4b5870c92d551333672ad3215218fe
Brandon Castro <castroyou999@gmail.com>
2023-03-11 23:47:50 -0500

    commit tree 02e75fb0dc1c5edab01e80836697db66b3542dec

--- src/features/cats/catsSlice.js
+++ src/features/cats/catsSlice.js
@@ -1,40 +1,75 @@
+import { createAsyncThunk , createSlice } from "@reduxjs/toolkit";
 // Action Creators
 
 // async actions
-export function fetchCats() {
-  return function (dispatch) {
-    dispatch({ type: "cats/fetchCats/pending" });
-    fetch("https://learn-co-curriculum.github.io/cat-api/cats.json")
-      .then((response) => response.json())
-      .then((data) => {
-        dispatch({
-          type: "cats/fetchCats/fulfilled",
-          payload: data.images,
-        });
-      });
-  };
-}
+// export function fetchCats() {
+//   return function (dispatch) {
+//     dispatch({ type: "cats/fetchCats/pending" });
+//     fetch("https://learn-co-curriculum.github.io/cat-api/cats.json")
+//       .then((response) => response.json())
+//       .then((data) => {
+//         dispatch({
+//           type: "cats/fetchCats/fulfilled",
+//           payload: data.images,
+//         });
+//       });
+//   };
+// }
 
 // sync actions added for demo purposes
-export function catAdded(newCat) {
-  return {
-    type: "cats/catAdded",
-    payload: newCat,
-  };
-}
+// export function catAdded(newCat) {
+//   return {
+//     type: "cats/catAdded",
+//     payload: newCat,
+//   };
+// }
 
-export function catUpdated(updatedCat) {
-  return {
-    type: "cats/catUpdated",
-    payload: updatedCat,
-  };
-}
+// export function catUpdated(updatedCat) {
+//   return {
+//     type: "cats/catUpdated",
+//     payload: updatedCat,
+//   };
+// }
 
 // Reducer
 const initialState = {
   entities: [], // array of cats
   status: "idle", // loading state
 };
+
+export const fetchCats = createAsyncThunk("cats/fetchCats", ()=> {
+  // return a promise containing the data we want
+  return fetch("https://learn-co-curriculum.github.io/cat-api/cats.json")
+    .then((response) => response.json())
+    .then((data) => data.images);
+});
+
+const catsSlice = createSlice({
+  name: "cats",
+  initialState,
+  reducers: {
+    catAdded(state,action){
+      // using createSlice allows mutation of state without actually mutating state
+      state.entities.push(action.payload);
+    },
+    catUpdated(state,action){
+      const cat = state.entities.find((cat)=> cat.id === action.payload.id);
+      cat.url = action.payload.url;
+    },
+  },
+  extraReducers: {
+    // handle async action types
+    [fetchCats.pending](state) {
+      state.status = "loading";
+    },
+    [fetchCats.fulfilled](state,action){
+      state.entities = action.payload;
+      state.status = "idle";
+    },
+  },
+});
+
+
 
 function catsReducer(state = initialState, action) {
   switch (action.type) {
@@ -69,5 +104,5 @@ function catsReducer(state = initialState, action) {
       return state;
   }
 }
-
+export const {catAdded, catUpdated} = catsSlice.actions;
 export default catsReducer;
--- src/store.js
+++ src/store.js
@@ -9,3 +9,13 @@ const store = configureStore({
 });
 
 export default store;
+// import { createStore, applyMiddleware } from "redux";
+// import thunkMiddleware from "redux-thunk";
+// import { composeWithDevTools } from "redux-devtools-extension";
+// import rootReducer from "./reducer";
+
+// const composedEnhancer = composeWithDevTools(applyMiddleware(thunkMiddleware));
+
+// const store = createStore(rootReducer, composedEnhancer);
+
+// export default store;
